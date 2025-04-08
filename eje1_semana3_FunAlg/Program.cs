aa05f31ace6d8a7d37c2918057d6b819aaf8ffeb
UCLAB612 <UCLAB612@UCLAB612>
2025-04-08 18:16:30 -0500

    Agregar archivos de proyecto.

---
+++ eje1_semana3_FunAlg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eje1_semana3_FunAlg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
