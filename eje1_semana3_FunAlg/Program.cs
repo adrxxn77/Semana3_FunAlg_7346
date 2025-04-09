984a0de727058881b56d026aaf76296a2ac2c483
antony <n00426922@upn.pe>
2025-04-08 19:15:29 -0500

    Eh terminado con exito mi primer Algoritmo de C# en VisualStudio2022

--- eje1_semana3_FunAlg/Program.cs
+++ eje1_semana3_FunAlg/Program.cs
@@ -10,10 +10,21 @@ namespace eje1_semana3_FunAlg
     {
         static void Main(string[] args)
         {
+            int n1;
+            int n2;
+            int suma;
 
+            //Console.ReadLine es igual a leer en Pseint
+            Console.WriteLine("ingrese n1: ");
+            n1 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("ingrese n2: ");
+            n2 = int.Parse(Console.ReadLine());
 
+            suma = n1 + n2;
+            Console.WriteLine("La suma es: " + suma);
 
+            Console.ReadKey();
         }
     }
 }
