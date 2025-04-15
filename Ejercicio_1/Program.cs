b7f47e75649200e8774aa1afb2402d1429822099
antony <n00426922@upn.pe>
2025-04-15 18:11:28 -0500

    semana 4 , 100/100

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Ingrsee el valor de a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de b: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b) 
+            {
+                c = a;
+                a = b;
+                b = c;
+
+            }
+            Console.WriteLine("Valor de a: " + a);
+            Console.WriteLine("Valor de b: " + b);
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2 ;
+            Console.WriteLine("Ingrese valor de n1: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese valor de n2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1<n2)
+            {
+                Console.WriteLine(n1+"-"+n2);
+            }
+            if (n2 < n1)
+            {
+                Console.WriteLine(n1+"-"+n2);
+            }
+            
+        }
+    }
+}
